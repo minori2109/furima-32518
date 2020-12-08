7d3b2a8908ca65910c0643a5939b289b2d77052b
minori2109 <minori.ss0921@gmail.com>
2020-12-08 19:57:28 +0900

    userモデルのバリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,18 +4,18 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
-
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)\w{6,}\z/.freeze
   validates :password, format: { with: VALID_PASSWORD_REGEX, message: 'Include both letters and numbers' }
 
   VALID_NAME_REGEX = /\A[ぁ-んァ-ン一-龥]+\z/.freeze
-  validates :last_name, presence: true, format: { with: VALID_NAME_REGEX, message: 'Full-width characters' }
-  validates :first_name, presence: true, format: { with: VALID_NAME_REGEX, message: 'Full-width characters' }
-
   VALID_NAME_KANA_REGEX = /\A[ァ-ヶー－]+\z/.freeze
-  validates :last_name_kana, presence: true, format: { with: VALID_NAME_KANA_REGEX, message: 'Full-width katakana characters' }
-  validates :first_name_kana, presence: true, format: { with: VALID_NAME_KANA_REGEX, message: 'Full-width katakana characters' }
 
-  validates :birthday, presence: true
+  with_options presence: true do
+    validates :nickname
+    validates :last_name, format: { with: VALID_NAME_REGEX, message: 'Full-width characters' }
+    validates :first_name, presence: true, format: { with: VALID_NAME_REGEX, message: 'Full-width characters' }
+    validates :last_name_kana, presence: true, format: { with: VALID_NAME_KANA_REGEX, message: 'Full-width katakana characters' }
+    validates :first_name_kana, presence: true, format: { with: VALID_NAME_KANA_REGEX, message: 'Full-width katakana characters' }
+    validates :birthday
+  end
 end
