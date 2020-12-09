29d04f73f021e87c789d35519d4033b97403a8ba
minori2109 <minori.ss0921@gmail.com>
2020-12-09 12:49:35 +0900

    Shipping_areaモデルの作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,6 +3,7 @@ class Item < ApplicationRecord
   belongs_to :category
   belongs_to :status
   belongs_to :shipping_fee
+  belongs_to :shipping_area
 
-  validates :category_id, :status_id, :shipping_fee_id, numericality: { other_than: 1 }
+  validates :category_id, :status_id, :shipping_fee_id, :shipping_area_id, numericality: { other_than: 1 }
 end
