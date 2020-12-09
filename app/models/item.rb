77fbd89e76e212f4da14564d598b59e6d89fbad3
minori2109 <minori.ss0921@gmail.com>
2020-12-09 12:33:37 +0900

    Statusモデルの作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
+  belongs_to :status
 
-  validates :category_id,  numericality: { other_than: 1 }
+  validates :category_id, :status_id, numericality: { other_than: 1 }
 end
