fd24964a865a677286f8982fc7c8a859443f82ce
minori2109 <minori.ss0921@gmail.com>
2020-12-11 19:01:21 +0900

    itemsコントローラーの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:show, :edit]
+  before_action :set_item, only: [:show, :edit, :update]
   before_action :move_to_index, only: :edit
 
   def index
@@ -27,8 +27,7 @@ class ItemsController < ApplicationController
   end
 
   def update
-    @item = Item.find(params[:id])
-    if @item.update(item_params)
+    if @item.user_id == current_user.id && @item.update(item_params)
       redirect_to item_path(params[:id])
     else
       render :edit
