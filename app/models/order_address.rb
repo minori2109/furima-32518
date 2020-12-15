a98be1086ae879698498f1edf1145d2e7819c498
minori2109 <minori.ss0921@gmail.com>
2020-12-15 11:20:57 +0900

    購入情報の登録機能の実装

---
+++ app/models/order_address.rb
@@ -0,0 +1,18 @@
+class OrderAddress
+  include ActiveModel::Model
+  attr_accessor :postcode, :shipping_area_id, :city, :block, :building, :phone_number, :user_id, :item_id
+
+  with_options presence: true do
+    validates :postcode, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "Input correctly" }
+    validates :city
+    validates :block
+    validates :phone_number, numericality: { only_integer: true, message: "Input only number" }
+  end
+
+  validates :shipping_area_id, numericality: { other_than: 1, message: "Select" }
+
+  def save
+    order = Order.create(user_id: user_id, item_id: item_id)
+    Address.create(postcode: postcode, shipping_area_id: shipping_area_id, city: city, block: block, building: building, phone_number: phone_number, order_id: order.id)
+  end
+end
